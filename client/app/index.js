fc5310a7a8f82ba3819371c92d5fd3de9db50330
PatriciaZan <patricia.zan.oliveira@gmail.com>
2024-10-11 18:20:00 -0300

    add files

---
+++ client/app/index.js
@@ -0,0 +1,32 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade'),
+]
+
+//console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo){
+        // cria uma td dem info
+        var td = document.createElement('td');
+        // atribui o valor do campo a td
+        td.textContent = campo.value;
+        // add a td na tr
+        tr.appendChild(td);
+    });
+
+    // nova td que armazenará o volume da negociação
+    var tdVolume = document.createElement('td');
+    // posição 1 -> quantidade
+    // posição 2 -> valor
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    // add td que faltava a tr
+    tr.appendChild(tdVolume);
+})
